763c6245f2fe9437c2817550bf0a45babd74193e
Valentin Kuznetsov <36416963+Gelios3031@users.noreply.github.com>
2018-02-28 18:55:06 +0200

    Create Triangle.java

---
+++ HomeWorkOOP1a/Triangle.java
@@ -0,0 +1,61 @@
+package com.gmail.gelios3031;
+
+public class Triangle {
+	private int a;
+	private int b;
+	private int c;
+
+	public Triangle(int a, int b, int c) {
+		super();
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	public Triangle() {
+		super();
+	}
+
+	public int getA() {
+		return a;
+	}
+
+	public void setA(int a) {
+		this.a = a;
+	}
+
+	public int getB() {
+		return b;
+	}
+
+	public void setB(int b) {
+		this.b = b;
+	}
+
+	public int getC() {
+		return c;
+	}
+
+	public void setC(int c) {
+		this.c = c;
+	}
+
+	@Override
+	public String toString() {
+		return "Triangle [a=" + a + ", b=" + b + ", c=" + c + "]";
+	}
+
+	public double getSquare() {
+		double p = (a + b + c) / 2;
+		double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+		return s;
+	}
+
+	public boolean chekTriangle() {
+		boolean Triangle = false;
+		if ((a + b > c) && (a + c > b) && (c + b > a)) {
+			Triangle = true;
+		}
+		return Triangle;
+	}
+}
